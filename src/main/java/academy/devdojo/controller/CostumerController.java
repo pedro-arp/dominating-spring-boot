369783ecb9b56d587029f5f0d2ca89dbe96fc2ac
Pedro Augusto <pedroagtrodrigues@gmail.com>
2023-12-06 13:52:03 -0300

    09-PathVariable

--- src/main/java/academy/devdojo/controller/CostumerController.java
+++ src/main/java/academy/devdojo/controller/CostumerController.java
@@ -1,9 +1,6 @@
 package academy.devdojo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,4 +30,12 @@ public class CostumerController {
     public List<String> filter(@RequestParam(name = "name") List<String> names) {
         return NAMES.stream().filter(names::contains).toList();
     }
+
+    @GetMapping("{name}")
+    public String findByName(@PathVariable String name){
+        return NAMES.stream().filter(n -> n.equalsIgnoreCase(name))
+                .findFirst()
+                .orElse("");
+    }
+
 }
