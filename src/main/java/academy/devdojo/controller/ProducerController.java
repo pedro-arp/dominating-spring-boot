f4320b2321c9eec86e864f1722dcb0745c64acbb
Pedro Augusto <pedroagtrodrigues@gmail.com>
2023-12-14 11:35:23 -0300

    15-ResponseEntity

--- src/main/java/academy/devdojo/controller/ProducerController.java
+++ src/main/java/academy/devdojo/controller/ProducerController.java
@@ -2,7 +2,9 @@ package academy.devdojo.controller;
 
 import domain.Producer;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProducerController {
     @PostMapping(value = "post", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-version=v1")
-    public Producer save(@RequestBody Producer producer) {
+    public ResponseEntity<Producer> save(@RequestBody Producer producer) {
         Producer lastObject = Producer.getProducers().get(Producer.getProducers().size() -1);
         producer.setId(lastObject.getId() + 1);
         Producer.getProducers().add(producer);
-        return producer;
+       return ResponseEntity.status(HttpStatus.CREATED).body(producer);
     }
 }
