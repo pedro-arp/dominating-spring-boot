275340b40a14297a0691b0c1f0eaae7bb18de3eb
pedro-arp <pedroagtrodrigues@gmail.com>
2024-02-06 11:40:16 -0300

    32 - Testes Unitários pts 03

--- src/test/java/academy/devdojo/repository/ProducerHardCodedRepositoryTest.java
+++ src/test/java/academy/devdojo/repository/ProducerHardCodedRepositoryTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,12 +23,13 @@ class ProducerHardCodedRepositoryTest {
     private ProducerData producerData;
 
     private List<Producer> producers;
+
     @BeforeEach
-    void init(){
+    void init() {
         var producer1 = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
         var producer2 = Producer.builder().id(2L).name("Wit Studio").createdAt(LocalDateTime.now()).build();
         var producer3 = Producer.builder().id(3L).name("Studio Ghibli").createdAt(LocalDateTime.now()).build();
-         producers = List.of(producer1, producer2, producer3);
+        producers = new ArrayList<>(List.of(producer1, producer2, producer3));
 
         BDDMockito.when(producerData.getProducers()).thenReturn(producers);
     }
@@ -35,28 +37,31 @@ class ProducerHardCodedRepositoryTest {
     @Test
     @DisplayName("findAll() must return a list of all producers")
     @Order(1)
-    void findAll_ReturnsAllProducers_WhenSucessful(){
+    void findAll_ReturnsAllProducers_WhenSuccessful() {
         var producers = repository.findAll();
         Assertions.assertThat(producers).hasSameElementsAs(this.producers);
     }
+
     @Test
     @DisplayName("findById() returns an producer when given Id")
     @Order(2)
-    void findById_ReturnProducerById_WhenSucessful(){
+    void findById_ReturnProducerById_WhenSuccessful() {
         var producerOptional = repository.findById(3L);
         Assertions.assertThat(producerOptional).contains(producers.get(2));
     }
+
     @Test
     @DisplayName("findByName() returns all Producers when name is null")
     @Order(3)
-    void findByName_ReturnAllProducers_WhenNameIsNull(){
+    void findByName_ReturnAllProducers_WhenNameIsNull() {
         var producers = repository.findByName(null);
         Assertions.assertThat(producers).hasSameElementsAs(this.producers);
     }
+
     @Test
     @DisplayName("findByName() returns list with filtered producers name is not null")
     @Order(4)
-    void findByName_ReturnFilteredProducers_WhenNameIsNotNull(){
+    void findByName_ReturnFilteredProducers_WhenNameIsNotNull() {
         var producers = repository.findByName("Ufotable");
         Assertions.assertThat(producers).hasSize(1).contains(this.producers.get(0));
     }
@@ -64,9 +69,39 @@ class ProducerHardCodedRepositoryTest {
     @Test
     @DisplayName("findByName() returns empty list of producers when nothing is found")
     @Order(5)
-    void findByName_ReturnEmptyList_WhenNothingIsFound(){
+    void findByName_ReturnEmptyList_WhenNothingIsFound() {
         var producers = repository.findByName("XXXX");
         Assertions.assertThat(producers).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("save() Create a producer")
+    @Order(6)
+    void save_CreatesProducer_WhenSuccessful() {
+        var producerToSave = Producer.builder().id(4L).name("Test").createdAt(LocalDateTime.now()).build();
+        var producer = repository.save(producerToSave);
+        Assertions.assertThat(producer).isEqualTo(producerToSave).hasNoNullFieldsOrProperties();
+        var producers = repository.findAll();
+        Assertions.assertThat(producers).contains(producerToSave);
+    }
+    @Test
+    @DisplayName("delete() Removes a producer")
+    @Order(7)
+    void delete_RemovesProducer_WhenSuccessful() {
+        var producerToDelete = this.producers.get(0);
+        repository.delete(producerToDelete);
+        Assertions.assertThat(this.producers).doesNotContain(producerToDelete);
+    }
+    @Test
+    @DisplayName("update() Update a producer")
+    @Order(8)
+    void update_UpdateProducer_WhenSuccessful() {
+        var producerToUpdate = this.producers.get(0);
+        producerToUpdate.setName("Test");
+        repository.update(producerToUpdate);
+        Assertions.assertThat(this.producers).contains(producerToUpdate);
+        this.producers.stream().filter(producer -> producer.getId().equals(producerToUpdate.getId())).findFirst()
+                .ifPresent(producer -> Assertions.assertThat(producer.getName()).isEqualTo(producerToUpdate.getName()));
     }
 
 }
