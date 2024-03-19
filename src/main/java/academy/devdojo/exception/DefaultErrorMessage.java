4b139ec068d9f74e9d2dc55e99397e327f124fa6
pedro-arp <pedroagtrodrigues@gmail.com>
2024-03-19 09:15:08 -0300

    70 - Criando modulo core compartilhado pt03 Integrating microservices by pattern exception

--- src/main/java/academy/devdojo/exception/DefaultErrorMessage.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.exception;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record DefaultErrorMessage(int status, String message) {
-}
