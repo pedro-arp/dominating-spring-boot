82ee45bbc272f08262ac898f7a3848ac31118657
pedro-arp <pedroagtrodrigues@gmail.com>
2024-05-10 10:55:31 -0300

    126 - Spring Security 07
    Autenticação e autorização com banco de dados :)

--- user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
+++ user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package academy.devdojo.config;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,11 +9,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -25,37 +19,30 @@ public class SecurityConfig {
 
     private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "v3/api-docs/**", "/swagger-ui/**", "/csrf"};
 
-    @Value("${user.username}")
-    private String usernameUser;
-
-    @Value("${user.password}")
-    private String passwordUser;
-
-    @Value("${admin.username}")
-    private String usernameAdmin;
-
-    @Value("${admin.password}")
-    private String passwordAdmin;
-
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        var user = User.withUsername(usernameUser)
-                .password(passwordEncoder.encode(passwordUser))
-                .roles("USER")
-                .build();
-
-        var admin = User.withUsername(usernameAdmin)
-                .password(passwordEncoder.encode(passwordAdmin))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+//    @Value("${user.username}")
+//    private String usernameUser;
+//    @Value("${user.password}")
+//    private String passwordUser;
+//    @Value("${admin.username}")
+//    private String usernameAdmin;
+//    @Value("${admin.password}")
+//    private String passwordAdmin;
+//    @Bean
+//    public UserDetailsService userDetails(PasswordEncoder passwordEncoder) {
+//        var user = User.withUsername(usernameUser)
+//                .password(passwordEncoder.encode(passwordUser))
+//                .roles("USER")
+//                .build();
+//        var admin = User.withUsername(usernameAdmin)
+//                .password(passwordEncoder.encode(passwordAdmin))
+//                .roles("ADMIN")
+//                .build();//        return new InMemoryUserDetailsManager(user, admin); }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(HttpMethod.POST, "v1/users").permitAll()
                         //.requestMatchers(HttpMethod.GET, "v1/users/list").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "v1/users/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "v1/users/**").hasRole("ADMIN")
@@ -67,8 +54,5 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+
 }
--- user-service/src/main/java/academy/devdojo/mapper/UserMapper.java
+++ user-service/src/main/java/academy/devdojo/mapper/UserMapper.java
@@ -1,18 +1,26 @@
 package academy.devdojo.mapper;
 
+import academy.devdojo.annotation.EncodedMapping;
 import academy.devdojo.domain.User;
 import academy.devdojo.request.UserPostRequest;
 import academy.devdojo.request.UserPutRequest;
 import academy.devdojo.response.UserGetResponse;
 import academy.devdojo.response.UserPostResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedSourcePolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedSourcePolicy = ReportingPolicy.IGNORE
+, uses = PasswordEncoderMapper.class)
 public interface UserMapper {
+
+    @Mapping(target = "roles", constant = "USER")
+
+    @Mapping(target = "password", qualifiedBy = EncodedMapping.class)
+
     User toUser(UserPostRequest request);
 
     UserPostResponse toUserPostResponse(User user);
