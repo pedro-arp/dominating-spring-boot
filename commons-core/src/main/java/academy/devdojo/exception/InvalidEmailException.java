f25d25341f7df7c4501a99810bf9e2a1cfff6f0c
pedro-arp <pedroagtrodrigues@gmail.com>
2024-08-08 10:51:54 -0300

    128 - 09
    129 - 10
    
    Atualizando Testes

--- commons-core/src/main/java/academy/devdojo/exception/InvalidEmailException.java
+++ commons-core/src/main/java/academy/devdojo/exception/InvalidEmailException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class InvalidEmailException extends ResponseStatusException {
+
     public InvalidEmailException(String message) {
         super(HttpStatus.BAD_REQUEST, message);
     }
--- user-service/src/test/resources/sql/user/init_three_users.sql
+++ user-service/src/test/resources/sql/user/init_three_users.sql
@@ -1,6 +1,6 @@
-insert into user (first_name, last_name, email)
-values ('Kiara', 'Violante', 'kiara@hotmail.com');
-insert into user (first_name, last_name, email)
-values ('Jolie', 'Augusta', 'jolie@hotmail.com');
-insert into user (first_name, last_name, email)
-values ('Bruce', 'Banner', 'bruce@hotmail.com');
+insert into user (first_name, last_name, email, roles, password)
+values ('Kiara', 'Violante', 'kiara@hotmail.com', 'USER', '{bcrypt}$2a$10$BmWgGrkptSleGv6mv.nQ0OJnPMf61vjZUTiX7m6bAh/xYhEKukUP2');
+insert into user (first_name, last_name, email, roles, password)
+values ('Jolie', 'Augusta', 'jolie@hotmail.com', 'USER', '{bcrypt}$2a$10$BmWgGrkptSleGv6mv.nQ0OJnPMf61vjZUTiX7m6bAh/xYhEKukUP2');
+insert into user (first_name, last_name, email, roles, password)
+values ('Bruce', 'Banner', 'bruce@hotmail.com', 'USER', '{bcrypt}$2a$10$BmWgGrkptSleGv6mv.nQ0OJnPMf61vjZUTiX7m6bAh/xYhEKukUP2');
