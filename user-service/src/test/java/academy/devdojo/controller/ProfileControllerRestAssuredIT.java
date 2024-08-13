fb91463f431fffcf0647ea8bc8f1b3f7d02da21e
pedro-arp <pedroagtrodrigues@gmail.com>
2024-08-13 09:46:19 -0300

    132 - Spring Security 13
    Atualizando Testes

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
@@ -23,10 +23,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlMergeMode;
 
 import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RestAssuredConfig.class)
+@Sql("/sql/user/init_one_login_regular_user.sql")
+@SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
 public class ProfileControllerRestAssuredIT extends IntegrationTestContainers {
     private static final String URL = "/v1/profiles";
 
---
+++ user-service/src/test/resources/sql/user/init_one_login_regular_user.sql
@@ -0,0 +1,2 @@
+insert into user (first_name, last_name, email, roles, password)
+values ('Kiara', 'Violante', 'kiara@hotmail.com', 'USER', '{bcrypt}$2a$10$BmWgGrkptSleGv6mv.nQ0OJnPMf61vjZUTiX7m6bAh/xYhEKukUP2');
