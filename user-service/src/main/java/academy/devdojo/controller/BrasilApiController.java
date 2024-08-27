3e3a1aa36f385b4f4e6cc2bebd10c82f1114d5c1
pedro-arp <pedroagtrodrigues@gmail.com>
2024-08-27 12:00:01 -0300

    148 - RestClient parte 01

--- user-service/src/main/java/academy/devdojo/controller/BrasilApiController.java
+++ user-service/src/main/java/academy/devdojo/controller/BrasilApiController.java
@@ -1,11 +1,14 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.config.BrasilApiConfigurationProperties;
+import academy.devdojo.response.CepGetResponse;
+import academy.devdojo.service.BrasilApiService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +21,16 @@ public class BrasilApiController {
 
     private final BrasilApiConfigurationProperties brasilApiConfigurationProperties;
 
-    @GetMapping
-    public ResponseEntity<Void> list() {
+    private final BrasilApiService brasilApiService;
+
+    @GetMapping("/{cep}")
+    public ResponseEntity<CepGetResponse> list(@PathVariable String cep) {
 
         log.info("Request received to find a Cep");
-        log.info("base-url {}, uri {}", brasilApiConfigurationProperties.baseUrl(), brasilApiConfigurationProperties.uri());
 
-        return ResponseEntity.noContent().build();
+        var cepFound = brasilApiService.findCep(cep);
+
+        return ResponseEntity.ok(cepFound);
 
     }
 
