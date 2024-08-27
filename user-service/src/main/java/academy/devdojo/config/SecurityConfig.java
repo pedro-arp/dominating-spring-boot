08a7cd7830bfd2459ae26a945a022e90a33d8310
pedro-arp <pedroagtrodrigues@gmail.com>
2024-08-27 11:03:59 -0300

    147 - Configuration Properties

--- user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
+++ user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "v3/api-docs/**", "/swagger-ui/**", "/csrf"};
+    private static final String[] WHITE_LIST = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/csrf"};
 
 //    @Value("${user.username}")
 //    private String usernameUser;
---
+++ user-service/src/main/java/academy/devdojo/controller/BrasilApiController.java
@@ -0,0 +1,31 @@
+package academy.devdojo.controller;
+
+import academy.devdojo.config.BrasilApiConfigurationProperties;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = {"v1/brasil-api/cep"})
+@RequiredArgsConstructor
+@Log4j2
+@SecurityRequirement(name = "basicAuth")
+public class BrasilApiController {
+
+    private final BrasilApiConfigurationProperties brasilApiConfigurationProperties;
+
+    @GetMapping
+    public ResponseEntity<Void> list() {
+
+        log.info("Request received to find a Cep");
+        log.info("base-url {}, uri {}", brasilApiConfigurationProperties.baseUrl(), brasilApiConfigurationProperties.uri());
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+}
